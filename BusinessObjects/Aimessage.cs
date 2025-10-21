91814ec112b590750a7f3648886a0cedc8ff8612
DaiLT <letandai1304@gmail.com>
2025-10-21 08:22:36 +0700

    set up project

---
+++ BusinessObjects/Aimessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects;
+
+public partial class Aimessage
+{
+    public int AimessageId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Sender { get; set; } = null!;
+
+    public string? MessageType { get; set; }
+
+    public string Content { get; set; } = null!;
+
+    public DateTime? SentAt { get; set; }
+
+    public bool? IsRead { get; set; }
+
+    public virtual Aiconversation User { get; set; } = null!;
+}
